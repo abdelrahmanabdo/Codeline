2faeb4aa0b57ad4db867e123c504177cc80b6525
Abdo Salmon <abdo@bodecanada.com>
2021-06-13 03:43:09 +0200

    - Structure project

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use('/public', express.static('resources'));
 
-
-var server = http.listen(3001, () => {
+const port = process.env.PORT || 3000;
+var server = http.listen(port, () => {
   console.log('Codeline server is running on port', server.address().port);
 });
---
+++ src/api/controllers/UserController.js
@@ -0,0 +1,43 @@
+module.exports = {
+
+  /**
+   * Returns user data from our database.
+   * 
+   * @param {Object?} data 
+   * @returns {Object}
+   * @public
+   */
+
+  async get(data) {},
+
+  /**
+   * Post user data from our database.
+   * 
+   * @param {Object?} data 
+   * @returns {Object}
+   * @public
+   */
+
+  async post(data) {},
+
+  /**
+   * Post user data from our database.
+   * 
+   * @param {Object?} data 
+   * @returns {Object}
+   * @public
+   */
+
+  async put(data) {},
+
+  /**
+   * Post user data from our database.
+   * 
+   * @param {Object?} data 
+   * @returns {Object}
+   * @public
+   */
+
+  async delete() {},
+
+}
