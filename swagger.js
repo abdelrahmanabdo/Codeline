ee167cca489c2ca896b7e5cbbe9b0e9acd7d7c53
Abdo Salmon <abdo@bodecanada.com>
2021-07-07 09:19:39 +0200

    - Update swagger ui

--- swagger.js
+++ swagger.js
@@ -4,10 +4,18 @@ module.exports = {
     info: {
       version: '1.0.0',
       title: 'Codeline API',
-      description: 'Codeline app APIs documentation.'
+      description: 'Codeline app APIs documentation.',
+      contact: {
+        name: 'Codeline Developer',
+        url: 'https://api.whatsapp.com/send?phone=+201096742196',
+        email: 'dev.abdelrahman1995@gmail.com'
+      },
     },
     servers: [
-      { url: 'https://app.codeline.social/api/v1/' },
+      { 
+        url: 'https://app.codeline.social/api/v1/',
+        description: "Development server"
+      },
       // { url: 'http://localhost:3000/api/v1/' }
     ],
     components: {
