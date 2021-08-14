e74364976e86f6eeb665e58222c087e80e24b992
Abdo Salmon <abdo@bodecanada.com>
2021-08-14 18:13:54 +0200

    - Make image name uuid

--- src/utils/db.js
+++ src/utils/db.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
 });
 
 connection.connect((error) => {
-  if (error) throw error;
+  if (error) return console.log('error when connecting to db:', error)
   console.log('Connection established');
 });
 
